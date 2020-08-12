86ac50cc7b0ab1a58e7342df6311f173e4792307
Alexandre Yembo <alexandreyembo@Alexandres-MacBook-Pro.local>
2020-08-12 17:07:45 +0100

    Adding BD project and SpecFlow references and configuration

---
+++ src/01-BDD/Hydra.BDD.Tests/Order/AddOrderItem.feature
@@ -0,0 +1,29 @@
+﻿Feature: Order - Add order item
+    As a user
+    I want to add this item to the order
+    So, that I can buy this item in the future
+
+Scenario: Add order item with sucess to a new order
+Given That a product is present in the showcase
+And Has stock available
+And The user is logged
+When The user add a new item to the basket
+Then The user will be redirected to order process
+And The amount of order will be the same of the same of the order item added
+
+Scenario: Add order item with fail to a new order
+Given That a product is present in the showcase
+And Has stock available
+And The user is logged
+When The user add a new item up to the stock available
+Then The user will receive a message that is allow to add more items that the limit specified
+
+Scenario: Add order item that is already present
+Given That a product is present in the showcase
+And Has stock available
+And The user is logged
+And The same product has been added to the order previously
+When The user add a new item to the basket
+Then The user will be redirected to order process
+And The quantity will be added to the product
+And The amount will consider the multiplcation of quantity and the product price
---
+++ src/01-BDD/Hydra.BDD.Tests/Order/Order_AddOrderItemSteps.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Hydra.BDD.Tests.Order
+{
+    [Binding]
+    public class Order_AddOrderItemSteps
+    {
+        [Given(@"Has stock available")]
+        public void GivenHasStockAvailable()
+        {
+           //Arrange
+
+            //Act
+
+            //Assert
+        }
+        
+        [Given(@"The user is logged")]
+        public void GivenTheUserIsLogged()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [Given(@"The same product has been added to the order previously")]
+        public void GivenTheSameProductHasBeenAddedToTheOrderPreviously()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [Given(@"That a product is present in the showcase")]
+        public void GivenThatAProductIsPresentInTheShowcase()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+
+        [When(@"The user add a new item to the basket")]
+        public void WhenTheUserAddANewItemToTheBasket()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [When(@"The user add a new item up to the stock available")]
+        public void WhenTheUserAddANewItemUpToTheStockAvailable()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [Then(@"The user will be redirected to order process")]
+        public void ThenTheUserWillBeRedirectedToOrderProcess()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [Then(@"The amount of order will be the same of the same of the order item added")]
+        public void ThenTheAmountOfOrderWillBeTheSameOfTheSameOfTheOrderItemAdded()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [Then(@"The user will receive a message that is allow to add more items that the limit specified")]
+        public void ThenTheUserWillReceiveAMessageThatIsAllowToAddMoreItemsThatTheLimitSpecified()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [Then(@"The quantity will be added to the product")]
+        public void ThenTheQuantityWillBeAddedToTheProduct()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [Then(@"The amount will consider the multiplcation of quantity and the product price")]
+        public void ThenTheAmountWillConsiderTheMultiplcationOfQuantityAndTheProductPrice()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+    }
+}
